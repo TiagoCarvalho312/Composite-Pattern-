5592482c9bfd10a9bee4f14802b9a94961c29f74
Tiago <tiagodcarvalho01@gmail.com>
2024-10-03 14:44:38 -0300

    Criação Classe TSqlUpdate
    Instrução de atualização de Banco de Dados

---
+++ TSQLuptade.class.php
@@ -0,0 +1,55 @@
+<?php 
+
+final class TSQLUptade extends TSqlInstruction{
+
+  private $columnValues;
+
+
+  public function setRowData($column,$value) {
+    //verifica se um dado e escalar
+    
+    if(is_scalar($value)) {
+
+        if(is_string($value) and (!empty($value))) {
+            
+            $value = addslashes($value);
+
+            $this->columnValues[$column] = "'$value'";
+
+        }
+        else if(is_bool($value)) {
+            $this->columnValues[$column] = $value ?'TRUE':'FALSE';
+        }
+        else if($value!==''){
+           $this->columnValues[$column] = $value;
+        }
+        else{
+            $this->columnValues[$column] = "NULL";
+        }
+    }
+  }
+  /*metodo getInstruction
+  retorna a instrução UPTADE em forma de string
+  */
+
+  public function getInstruction() {
+    //monta a string UPTADE
+
+    $this->sql = "UPTADE {$this->entity}";
+
+    //monta os pares: coluna=valor
+
+    if($this->columnValues){
+        foreach($this->columnValues as $column=>$value){
+
+            $set[] = "{$column} = {$value}";
+        }
+    }
+    $this->sql .= 'SET' . implode (',', $set);
+
+    if($this->criteria){
+        $this->sql .= 'WHERE' . $this->criteria->dump();
+    }
+  }
+}
+?>
